31d95683abba83fcbaf265339d63d64ace1471cc
Joshua <joshuadelgado073@gmail.com>
2025-12-17 03:12:16 +0800

    Update DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,       // nozomi.proxy.rlwy.net
-  port: process.env.DB_PORT,       // 16266
+  host: process.env.DB_HOST,       
+  port: process.env.DB_PORT,       
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
