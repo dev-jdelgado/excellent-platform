67510ae496b03d299fe6537d97b280e9b5c2e2cf
Joshua <joshuadelgado073@gmail.com>
2025-12-17 02:50:01 +0800

    Updated DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql2');
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('DB connection failed:', err);
+  } else {
+    console.log('MySQL connected');
+  }
 });
 
 module.exports = db;
