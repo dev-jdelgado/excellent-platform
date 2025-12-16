37433635220d7a1935d254ed1f20e6cfc23537a0
Joshua <joshuadelgado073@gmail.com>
2025-12-17 03:41:50 +0800

    Update Server file

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const authRoutes = require('./routes/auth.routes');
 const app = express();
 
 // CORS
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173'];
+const allowedOrigins = [process.env.FRONTEND_URL];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
