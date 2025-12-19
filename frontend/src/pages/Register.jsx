7d32c9e829be82ae915d53b8d9d4ffbb19d24742
Joshua <joshuadelgado073@gmail.com>
2025-12-20 03:02:05 +0800

    Change Logo

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -30,23 +30,11 @@ export default function Register() {
                 onSubmit={submit}
                 class="bg-white sm:px-8 px-5 py-8 rounded-xl shadow-lg w-full max-w-md flex flex-col items-center mx-4"
             >
-                <div className="w-16 mb-4 bg-primary rounded-2xl p-2 text-white">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-                        />
-                    </svg>
+                <div class="w-20 mb-4">
+                    <img src="./EXCELlent-Icon-Transparent.png"/>
                 </div>
                 <h1 class="sm:text-3xl text-xl font-bold text-black mb-6 text-center">
-                    Welcome to EXCELlent!
+                    Welcome to EXCELLENT!
                 </h1>
                 <div class="w-full">
                     <div class="flex flex-row items-center gap-1 mb-1">
