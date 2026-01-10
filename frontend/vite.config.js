9a65d803bbcf0e996edf4d4d49ed75a56f380b36
Joshua <joshuadelgado073@gmail.com>
2026-01-10 16:16:19 +0800

    Fixed Code 404 on Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    base: "/", // ensures all assets work in production
     server: {
         proxy: {
         "/api": {
